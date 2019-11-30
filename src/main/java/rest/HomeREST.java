4b59e72c8f47a18acac8e4340b68e62038c96f33
Tarang Parikh <tp0265@gmail.com>
2019-11-30 06:29:48 +0530

    #5

--- src/main/java/rest/HomeREST.java
+++ src/main/java/rest/HomeREST.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import dao.UserDAO;
 import vo.UserVO;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.*;
@@ -45,5 +46,15 @@ public class HomeREST {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getList(){
         return Response.ok(gson.toJson(userDAO.getList()),MediaType.APPLICATION_JSON).build();
+    }
+
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response post(String jsonData){
+        UserVO userVO = gson.fromJson(jsonData,UserVO.class);
+        Integer id = userDAO.insert(userVO);
+        return Response.ok(gson.toJson(new Status<>(id)),MediaType.APPLICATION_JSON).build();
     }
 }
